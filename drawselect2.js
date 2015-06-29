42df7ed5048872b68b082fa015bc9a276a4ddfb7
phiree <phiree@gmail.com>
2015-06-30 01:56:42 +0800

    '解决其他按钮时间无法触发的问题'

--- drawselect2.js
+++ drawselect2.js
@@ -1,31 +1,34 @@
 /*
-插件名称: DrawSelect
-环境: jQuery.
-描述:
-    鼠标框选 页面元素.
+ 插件名称: DrawSelect
+ 环境: jQuery.
+ 描述:
+ 鼠标框选 页面元素.
  传入对象:
-     可以被选择的元素集合.
+ 可以被选择的元素集合.
  用法:
- 
+
  $(".can_be_selected_div").DrawSelect({
-   on_select_complete:function(selected_elements){
-        for(var s in selected_elements)
-        ..........
-        .......
-   }
+ on_select_complete:function(selected_elements){
+ for(var s in selected_elements)
+ ..........
+ .......
+ }
  });
-  2015-6-27 
-*/
+ 2015-6-27
+ */
 
 $.fn.DrawSelect = function (options) {
     var params = $.extend({
-        'selected_class':'selected',                //本次选中元素的样式
-        'drawer_class':'draw',                      //框的样式.
-        'interval_span':100,                          //计时器循环间隔, 单位:毫秒
-        'on_select_complete': function(){}     //框选结束(鼠标弹起)后的调用的函数,  传入已选元素的jquery对象列表$(selected_class), 供调用方使用.
+        'container':$(document),//包含所有需要框选元素的 容器, 默认是 document.
+        'selectable':'selectable', //可以被选取的元素的样式名称.
+        'selected_class': 'selected',                //被选中元素的样式
+        'drawer_class': 'draw',                      //框的样式.
+        'interval_span': 100,                          //计时器循环间隔, 单位:毫秒
+        'on_select_complete': function () {
+        }     //框选结束(鼠标弹起)后的调用的函数,  传入已选元素的jquery对象列表$(selected_class), 供调用方使用.
     }, options);
-    
-    var selectableDivs = this;
+    var container=params.container;
+    var selectableDivs = $('.'+params.selectable);
     var is_mouse_down = false;
     var lastDrawDivStartPosition = {left: 0, top: 0};
     //将div相关数值放入变量, 供遍历比较,替代直接遍历div引起的性能问题.
@@ -39,15 +42,17 @@ $.fn.DrawSelect = function (options) {
         var data = {index: c, left: left, top: top, outerHeight: outerHeight, outerWidth: outerWidth};
         dataList.push(data);
     }
-    
+
     //鼠标按下,计时器开始,判断画框 和元素的重叠性
     var timeInteval = null;
-    $(window).mousedown(function (e) {
+
+    container.mousedown(function (e) {
+
         is_mouse_down = true;
         //创建划框div
-        var drawDiv = $("."+params.drawer_class);
+        var drawDiv = $("." + params.drawer_class);
         if (drawDiv.length == 0) {
-            drawDiv = $("<div class='"+params.drawer_class+"' style='position:absolute'></div>");
+            drawDiv = $("<div class='" + params.drawer_class + "' style='position:absolute'></div>");
             $(document.body).append(drawDiv);
         }
         lastDrawDivStartPosition.left = e.pageX;//划框的起始位置
@@ -60,6 +65,7 @@ $.fn.DrawSelect = function (options) {
         });
         //阻止默认框选事件
         e.preventDefault();
+
         //循环比较是否被框中.
         timeInteval = setInterval(ChangeSelectedStatus, params.interval_span);
 
@@ -68,15 +74,16 @@ $.fn.DrawSelect = function (options) {
     $(window).mouseup(function (e) {
         clearInterval(timeInteval);
         selectableDivs.removeAttr('fixed');
-        $('.'+params.selected_class).attr('fixed', 'fixed')
+        $('.' + params.selected_class).attr('fixed', 'fixed')
         is_mouse_down = false;
+          $("." + params.drawer_class).remove();
         //选中结束
-        params.on_select_complete($('.'+params.selected_class));
+        params.on_select_complete($('.' + params.selected_class));
     });
     //鼠标移动,修改画框大小
-    $(window).mousemove(function (e) {
+    container.mousemove(function (e) {
         if (is_mouse_down) {
-            var lastDrawDiv = $('.'+params.drawer_class);
+            var lastDrawDiv = $('.' + params.drawer_class);
             var width = e.pageX - lastDrawDivStartPosition.left;
             var height = e.pageY - lastDrawDivStartPosition.top;
             var width_abs = Math.abs(width);
@@ -87,7 +94,7 @@ $.fn.DrawSelect = function (options) {
             if (height < 0) {
                 lastDrawDiv.css({top: e.pageY});
             }
-            lastDrawDiv.css({width: width_abs  + 'px', height: height_abs  + 'px'});
+            lastDrawDiv.css({width: width_abs + 'px', height: height_abs + 'px'});
         }
         e.preventDefault();
     });
@@ -117,8 +124,8 @@ $.fn.DrawSelect = function (options) {
 
 //更改 选中状态.
     function ChangeSelectedStatus() {
-        
-        var last_draw_div = $('.'+params.drawer_class);
+
+        var last_draw_div = $('.' + params.drawer_class);
         var draw_div_data = {
             left: last_draw_div.offset().left,
             top: last_draw_div.offset().top,
@@ -132,11 +139,11 @@ $.fn.DrawSelect = function (options) {
             if (overlayed) {
                 //如果是之前选中的
                 if ($div.attr('fixed') == "fixed") {
-                    if($div.hasClass(params.selected_class)) {
+                    if ($div.hasClass(params.selected_class)) {
                         $div.removeClass(params.selected_class);
-                    } 
+                    }
                 }
-                else{
+                else {
                     $div.addClass(params.selected_class);
                 }
             }
